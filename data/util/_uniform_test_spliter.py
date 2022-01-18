6a457401e12302be07a7c5a8660e3148d75e3ecf
meo-s <kmeos1579@gmail.com>
2022-01-18 18:19:27 +0900

    Update annotation of _uniformly_distribute()

--- data/util/_uniform_test_spliter.py
+++ data/util/_uniform_test_spliter.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def _uniformly_distribute(n_samples: int, n_sections: int) -> np.ndarray:
     if n_sections <= 0 or n_samples < n_sections:
-        raise ValueError('n_sections does not fit to n_samples.'
+        raise ValueError('n_sections does not fit to n_samples.\n'
                          'n_samples=%d, n_sections=%d were given.' %
                          (n_samples, n_sections))
 
