d7275f588f036f4c1272ba2f41546d40dde1d788
meo-s <kmeos1579@gmail.com>
2022-01-20 16:58:41 +0900

    Refactor _validate_uniform_test_split_size()

--- data/util/_uniform_test_spliter.py
+++ data/util/_uniform_test_spliter.py
@@ -17,6 +17,7 @@ def _validate_uniform_test_split_size(
     """
 
     n_samples = len(label)
+    n_classes = len(np.unique(label))
     n_test = test_size
     n_train = train_size
 
@@ -29,24 +30,29 @@ def _validate_uniform_test_split_size(
             raise ValueError('The value of test_size argument must be between '
                              '0 and 1 when its type is float.')
         n_test = min(math.ceil(n_samples * test_size), n_samples)
-    if train_size is None:
-        n_train = n_samples - n_test
 
     if isinstance(train_size, float):
         if train_size < 0 or 1 < train_size:
             raise ValueError('The value of train_size argument must be between '
                              '0 and 1 when its type is float.')
         n_train = min(math.ceil(n_samples * train_size), n_samples)
+
     if test_size is None:
         n_test = n_samples - n_train
+        n_test -= n_test % n_classes
+
+    n_test -= n_test % n_classes  # Make n_test always multiples of n_classes.
+
+    if train_size is None:
+        n_train = n_samples - n_test
+
+    spc = [*dict(Counter(label)).values()]
+    if min(spc) < (n_test // n_classes):
+        raise ValueError('Number of samples per class is not enough to split.')
 
     if n_samples < (n_train + n_test):
         raise ValueError('Samples are not enough to split.\n'
                          'test_size={}, train_size={} were given.'.format(
                              test_size, train_size))
-
-    spc = [*dict(Counter(label)).values()]
-    if min(spc) < max(_uniformly_distribute(n_test, len(np.unique(label)))):
-        raise ValueError('Number of samples per class is not enough to split.')
 
     return n_train, n_test
