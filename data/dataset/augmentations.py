b11e60b1e305735909af33a65b3e5b7fe8d00a7c
meo-s <kmeos1579@gmail.com>
2022-03-03 10:29:01 +0900

    Add TranslateXAbs class

--- data/dataset/augmentations.py
+++ data/dataset/augmentations.py
@@ -90,3 +90,15 @@ class TranslateY(_BaseAugmentationOp):
         m *= img.size[1]
         return img.transform(img.size, PIL.Image.AFFINE, (1, 0, 0, 0, 1, m))
 
+
+class TranslateXAbs(_BaseAugmentationOp):
+
+    def __init__(self,
+                 magnitude_limit: Tuple[float, float] = (0, 10),
+                 random_mirror: bool = True):
+        super().__init__(magnitude_limit=magnitude_limit,
+                         random_mirror=random_mirror)
+
+    def _apply_transformation(self, img: PILImage, m: float) -> PILImage:
+        return img.transform(img.size, PIL.Image.AFFINE, (1, 0, m, 0, 1, 0))
+
