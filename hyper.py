a1b3c62ce9ec992b34d83b311707552b82e7508a
meo-s <kmeos1579@gmail.com>
2022-04-02 14:55:48 +0900

    Add additional hyper parameters

--- hyper.py
+++ hyper.py
@@ -2,12 +2,18 @@ from typing import Dict, Literal, Union
 
 import yaml
 
-HyperParameters = Dict[Union[Literal['aug.rand_augment.use'],
-                             Literal['aug.rand_augment.N'],
-                             Literal['aug.rand_augment.M']], Union[float, int,
-                                                                  str]]
+HyperParameterNames = Union[Literal['model'],
+                            Literal['n_epochs'],
+                            Literal['sz_batch'],
+                            Literal['optimizer'],
+                            Literal['SGD.base_lr'],
+                            Literal['SGD.momentum'],
+                            Literal['aug.rand_augment.use'],
+                            Literal['aug.rand_augment.N'],
+                            Literal['aug.rand_augment.M']]
+HyperParameters = Dict[HyperParameterNames, Union[float, int, str]]
 
 
 def load(file_path: str) -> HyperParameters:
-    with open(file_path, mode='r', encoding='utf-8') as f:  # pylint: disable=invalid-name
+    with open(file_path, mode='r', encoding='utf-8') as f:
         return yaml.load(f, Loader=yaml.CLoader)
