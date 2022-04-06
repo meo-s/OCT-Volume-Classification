b4a1ed0ada57a11ba5e8cecc4fb26bdd7daa13b2
meo-s <kmeos1579@gmail.com>
2022-04-07 03:35:39 +0900

    Add Transpose layer class

--- gnuoct/models/layers.py
+++ gnuoct/models/layers.py
@@ -1,1 +1,12 @@
+import torch
+import torch.nn as nn
 
+
+class Transpose(nn.Module):
+
+    def __init__(self, *dims):
+        super().__init__()
+        self.dims = dims
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return x.transpose(*self.dims)
