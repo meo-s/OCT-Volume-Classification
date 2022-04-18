2bd84a798cd249549e4ce8c0ab491be4cabe4f97
meo-s <kmeos1579@gmail.com>
2022-04-18 19:42:19 +0900

    Refactor train logic

--- train.py
+++ train.py
@@ -9,14 +9,12 @@ import os
 import random
 import sys
 import warnings
-from datetime import datetime
 from collections import defaultdict
 from typing import Any, Callable, Dict, List, Literal, Optional, Tuple, Union
 
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.utils.data
 import torchvision.transforms as tv
 import yaml
@@ -322,74 +320,160 @@ def evaluate(
     return total_eq / total, total_loss / len(eval_dtaldr)
 
 
-def _train(log_dir: str, dataset_path: str, hp: hyper.HyperParameters):
-    now = datetime.now().strftime('%Y%m%d_%a_%Hh%Mm%Ss')
-    log_dir = os.path.join(log_dir, now)
-    os.makedirs(log_dir, exist_ok=True)
+def _get_text_graph(values: Tuple[float], to_string: str) -> str:
+    if len(values) == 0:
+        return '-'
 
-    hp_str = yaml.dump(hp)
+    text_graph = to_string(values[0])
+    for i in range(1, len(values)):
+        text_graph += '  ↗ ' if values[i - 1] < values[i] else ''
+        text_graph += '  ↘ ' if values[i] < values[i - 1] else ''
+        text_graph += '  → ' if values[i - 1] == values[i] else ''
+        text_graph += to_string(values[i])
 
-    train_hp_path = os.path.join(log_dir, 'hp.yaml')
-    with open(train_hp_path, mode='w', encoding='utf-8') as f:
-        f.write(hp_str)
+    return text_graph.strip()
 
+
+def _save_ckpt(
+    ckpt_path: str,
+    model: nn.Module,
+    optimzier: torch.optim.Optimizer,
+    epoch: int,
+    history: Dict[str, List[float]],
+):
+    ckpt = {
+        'srs': random.getstate(),
+        'nrs': np.random.get_state(),
+        'trs': torch.random.get_rng_state(),
+        'model': model.state_dict(),
+        'optimizer': optimzier.state_dict(),
+        'epoch': epoch,
+        'history': history,
+    }
+
+    if not ckpt_path.endswith('.ckpt'):
+        ckpt_path = os.path.join(ckpt_path, '.ckpt')
+
+    torch.save(ckpt, ckpt_path)
+
+
+def _load_ckpt(
+    ckpt_path: str,
+    model: Optional[nn.Module] = None,
+    optimizer: Optional[torch.optim.Optimizer] = None,
+    sync_random_state: bool = False,
+) -> Dict[str, Any]:
+
+    if not ckpt_path.endswith('.ckpt'):
+        ckpt_path = os.path.join(ckpt_path, '.ckpt')
+
+    ckpt = torch.load(ckpt_path)
+
+    if sync_random_state:
+        random.setstate(ckpt['srs'])
+        np.random.set_state(ckpt['nrs'])
+        torch.random.set_rng_state(ckpt['trs'])
+
+    if model is not None:
+        model.load_state_dict(ckpt['model'])
+
+    if optimizer is not None:
+        optimizer.load_state_dict(ckpt['optimizer'])
+
+    if None not in (model, optimizer):
+        device = next(model.parameters()).device
+        for state in optimizer.state.values():
+            for k, v in state.items():
+                if isinstance(v, torch.Tensor):
+                    state[k] = v.to(device)
+
+    return ckpt
+
+
+def train(
+    log_dir: str,
+    dataset_path: str,
+    resume: bool,
+    hp: hyper.HyperParameters,
+):
     train_tag = get_train_tag(hp)
+    log_dir = os.path.join(log_dir, train_tag)
+    if not resume and os.path.exists(log_dir):
+        print(
+            'Training that had been performed with same hyperparameters '
+            'is detected.',
+            file=sys.stderr)
+        return
+
+    os.makedirs(log_dir, exist_ok=True)
+    with open(os.path.join(log_dir, '.hp.yaml'), 'w', encoding='utf-8') as f:
+        f.write(yaml.dump(hp))
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model = get_model(hp)
     model = model.to(device, non_blocking=True)
+    if hasattr(model, 'init_weights'):
+        model.init_weights()
 
     optimizer = get_optimizer(model.parameters(), hp)
 
     pad_fn = functools.partial(pad_chunk, target_size=32)
     dtaldrs = create_dtaldrs(dataset_path, hp, pad_fn=pad_fn)
-    # temporay
-    dtaldrs['val'] = dtaldrs['test']
 
-    acc_train, acc_val, acc_test = 0, 0, 0
-    with tqdm(total=hp['n_epochs'], initial=0, unit='epoch') as pbar:
-        while pbar.n < pbar.total:
-            _clear_terminal()
-            print(f'Last validation accuracy = {acc_val:.2%}')
-            pbar.set_description(f'* Epoch {pbar.n + 1:3d}')
+    label_smoothing = hp.setdefault('label_smoothing', 0.)
+    criterion = nn.CrossEntropyLoss(label_smoothing=label_smoothing)
+    criterion = criterion.to(device)
 
-            model.train()
-            acc_train = 0
-            train_pbar = tqdm(dtaldrs['train'], desc='* Training', leave=False)
-            for x, y, _ in train_pbar:
-                x = x.to(device, non_blocking=True)
-                y = y.to(device, non_blocking=True)
+    epoch, history = 1, defaultdict(list)
+    if resume:
+        ckpt = _load_ckpt(log_dir, model, optimizer, sync_random_state=True)
+        epoch, history = ckpt['epoch'] + 1, ckpt['history']
 
-                y_pred = model(x)  # pylint: disable=not-callable
-                optimizer.zero_grad()
-                loss = F.nll_loss(F.log_softmax(y_pred, dim=1), y)
-                loss.backward()
-                optimizer.step()
+    # pylint: disable=invalid-name
+    purge_step = len(dtaldrs['train']) * epoch
+    tb = utils.TrickySummaryWriter(log_dir, purge_step=purge_step)
+    for epoch in range(epoch, hp['n_epochs'] + 1):
+        _clear_terminal()
 
-                acc_train += (y_pred.argmax(dim=1).eq(y).sum().item() /
-                              len(dtaldrs['train'].dataset))
+        print('EPOCH', f'{epoch}/{hp["n_epochs"]}', sep='\t')
+        print('* TRAIN_TAG', train_tag, sep='\t')
 
-                train_pbar.set_description('* Training')
+        for metric_name in ('acc_train', 'loss_train', 'acc_val', 'loss_val'):
 
-            model.eval()
-            acc_val = 0
-            for x, y, _ in tqdm(dtaldrs['val'],
-                                desc='* Evaluating',
-                                leave=False):
-                x = x.to(device, non_blocking=True)
-                y = y.to(device, non_blocking=True)
+            def _to_string(metric_name: str) -> Callable[[float], str]:
+                is_acc = metric_name.startswith('acc')
+                vformat = ('{:.6f}', '{:6.4%}')[int(is_acc)]
+                return vformat.format
 
-                y_pred = model(x)  # pylint: disable=not-callable
-                acc_val += (y_pred.argmax(dim=1).eq(y).sum().item() /
-                            len(dtaldrs['val'].dataset))
+            text_graph = _get_text_graph(history[metric_name][-6:],
+                                         _to_string(metric_name))
+            print(f'* {metric_name.upper()}', text_graph, sep='\t')
 
-            pbar.update()
+        # yapf: disable
+        acc_train, loss_train = train_once(dtaldrs['train'], model, criterion, optimizer, hp)
+        # yapf: enable
+        acc_val, loss_val = evaluate(dtaldrs['test'], model, criterion)
+
+        history['acc_train'].append(acc_train)
+        history['loss_train'].append(loss_train)
+        history['acc_val'].append(acc_val)
+        history['loss_val'].append(loss_val)
+
+        for metric_name in ('acc_train', 'loss_train', 'acc_val', 'loss_val'):
+            tb.add_scalar(metric_name.replace('_', '/'),
+                          history[metric_name][-1],
+                          len(dtaldrs['train']) * epoch)
+
+        _save_ckpt(log_dir, model, optimizer, epoch, history)
+        if max(*history['acc_val']) <= acc_val:
+            pass
 
 
 def _main(
     log_dir: str,
     dataset_path: str,
     hyper_path: str,
+    resume: bool,
     **_,
 ):
     if not os.path.exists(dataset_path):
@@ -397,8 +481,10 @@ def _main(
               file=sys.stderr)
         sys.exit(0)
 
+    if resume and not hyper_path.endswith(('.yaml', '.yml')):
+        hyper_path = os.path.join(log_dir, hyper_path, '.hp.yaml')
     if not os.path.exists(hyper_path):
-        print(f'A hyper parameter settings file "{hyper_path}" does not exist.',
+        print(f'A hyperparameter file "{hyper_path}" does not exist.',
               file=sys.stderr)
         sys.exit(0)
 
@@ -407,15 +493,10 @@ def _main(
         with open(os.path.join(_CACHE_DIR, _PRT_MODEL), 'wb') as f:
             utils.request_url_content(_PRT_MODEL_URL, f)
 
-    hp = hyper.load(hyper_path)
-
-    os.makedirs(log_dir, exist_ok=True)
-
-    mp_context = mp.get_context('spawn')
-    training_process = mp_context.Process(target=_train,
-                                          args=(log_dir, dataset_path, hp))
-    training_process.start()
-    training_process.join()
+    train(log_dir=log_dir,
+          dataset_path=dataset_path,
+          resume=resume,
+          hp=hyper.load(hyper_path))
 
 
 if __name__ == '__main__':
@@ -424,6 +505,7 @@ if __name__ == '__main__':
     arg_parser.add_argument('--log_dir', '-l', dest='log_dir', type=str, default='log')
     arg_parser.add_argument('--dataset', '-d', dest='dataset_path', type=str, required=True)
     arg_parser.add_argument('--hyper', dest='hyper_path', type=str, required=True)
+    arg_parser.add_argument('--resume', '-r', dest='resume', action='store_true')
     # yapf: enable
 
     args = vars(arg_parser.parse_args())
